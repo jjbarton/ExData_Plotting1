43117d4b2afef0f6ff3ba1a5678f24f151f7a083
unknown <jbarton@WGSB-JBAR02.bhdgsystematic.com>
2014-12-04 12:46:34 +0000

    Modified plot 2 graph.

--- plot2.R
+++ plot2.R
@@ -5,7 +5,7 @@ source("helper.R")
 consume.data <- loadConsumptionData()
 
 # Create the chart
-with(consume.data, plot(Time, Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)"))
+with(consume.data, plot(Time, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)"))
 
 # Copy the chart to a PNG file using helper function
 copyToPng("plot2.png")
